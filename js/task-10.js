c64df70f6ac58c28f95aac68c2dc7dcfb0bfe67e
GubarievaAnna <annagubarieva@gmail.com>
2022-06-26 10:44:33 +0300

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonCreateEl = document.querySelector("button[data-create]");
+const buttonDestroyEl = document.querySelector("button[data-destroy]");
+const inputEl = document.querySelector("input");
+const divForFillEl = document.querySelector("#boxes");
+
+buttonCreateEl.addEventListener("click", () => {
+  if (inputEl.value < 1) {
+    return alert("Input value > 0");
+  }
+  return createBoxes(inputEl.value);
+});
+
+function createBoxes(amount) {
+  let width = 20;
+  let height = 20;
+  for (let i = 1; i <= amount; i++) {
+    const divEl = document.createElement("div");
+    divEl.style.width = `${(width += 10)}px`;
+    divEl.style.height = `${(height += 10)}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    divForFillEl.append(divEl);
+  }
+}
+buttonDestroyEl.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  divForFillEl.innerHTML = "";
+}
